97e4a72b134fe3c7da11e4a90e5d5b0c36af9721
JBOSS - Vitor Bersani <desenvolvimento07@jboss.srv.br>
2022-03-04 11:05:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -44,9 +44,9 @@ app.post('/', (req, res) => {
     console.log('-----------------------')
 
 
-    axios.post('https://webhook.site/b189fa53-ef42-492e-b4e7-98f6a5a31d38', req.body)
+    /*axios.post('https://webhook.site/b189fa53-ef42-492e-b4e7-98f6a5a31d38', req.body)
         .then(() => { })
-    console.log('=> Requisição enviada para fora')
+    console.log('=> Requisição enviada para fora')*/
 
     handleCall(req.body, res);
 
